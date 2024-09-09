f662d5f5d8860e4b894e3238af5433e0d2c6dfd5
grnqrtr <grnqrtr@protonmail.com>
2024-09-09 14:52:59 +0900

    added alias

--- home.nix
+++ home.nix
@@ -6,6 +6,7 @@ let
     ll = "ls -l";
     la = "ls -la";
     ".." = "cd ..";
+    config = "cd /home/grnqrtr/.nixos-config";
     flake-update = "nix flake update /home/grnqrtr/.nixos-config/";
     home-switch = "home-manager switch --flake /home/grnqrtr/.nixos-config/";
     nix-rebuild = "sudo nixos-rebuild switch --flake /home/grnqrtr/.nixos-config/#t480s";
