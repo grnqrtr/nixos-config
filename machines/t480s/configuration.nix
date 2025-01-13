470b9cafccaa9d53a24509d44e89f0eec6c01d0d
installer <installer@gmail.com>
2025-01-13 16:40:14 +0900

    temporarily add firefox, added fcitx

--- machines/t480s/configuration.nix
+++ machines/t480s/configuration.nix
@@ -87,10 +87,21 @@
   };
 
   # Enable Japanese input using ibus and Mozc
+#  i18n.inputMethod = {
+#    enable = true;
+#    type = "ibus";
+#    ibus.engines = with pkgs.ibus-engines; [ mozc ];
+# };
+
+  # Enable Japanese input using fcitx5 and mozc
   i18n.inputMethod = {
     enable = true;
-    type = "ibus";
-    ibus.engines = with pkgs.ibus-engines; [ mozc ];
+    type = "fcitx5";
+    fcitx5.waylandFrontend = true;
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
   };
 
   # Enable the X11 windowing system.
