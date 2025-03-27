f585e8c375260212bff352c1c62f001992c62e78
installer <installer@gmail.com>
2025-03-27 16:54:57 +0900

    made style module

--- users/grnqrtr/home.nix
+++ users/grnqrtr/home.nix
@@ -12,6 +12,7 @@ in
 
   # Import other modules
   imports = [
+    ./style.nix
     ../../hm-modules/librewolf.nix
     ../../hm-modules/flatpak.nix
     ../../hm-modules/shell/zsh.nix
@@ -28,45 +29,6 @@ in
       init.defaultBranch = "master";
     };
   };
-
-  # Styling Options
-  stylix = {
-    enable = true;
-    image = ./wallpaper-moon.jpg;
-    polarity = "dark";
-    opacity.terminal = 0.8;
-    cursor.package = pkgs.bibata-cursors;
-    cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 24;
-    targets.vscode.profileNames = [ "default" ];
-    targets.librewolf.profileNames = [ "Default" ];
-    fonts = {
-      monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono Nerd Font Mono";
-      };
-      sansSerif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      serif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      sizes = {
-        applications = 12;
-        terminal = 15;
-        desktop = 11;
-        popups = 12;
-      };
-    };
-  };
-
-  # Workaround to set active hint color
-  dconf.settings."org/gnome/shell/extensions/pop-shell" = {
-    hint-color-rgba = "rgba(0, 255, 255, 1)";
-  };
-
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
