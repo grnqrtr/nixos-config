9138885efe66db40a918f67832020d410ebae361
installer <installer@gmail.com>
2025-03-27 23:24:07 +0900

    flake update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    stableNixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    stableNixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
